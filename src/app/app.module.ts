2e83b5a5f5c368d3df8aa47dfebf525d962ca93a
karolyn <dayita1204@gmail.com>
2023-04-13 09:08:15 -0500

    Add proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routes/views/home/home.component';
+import { HeaderComponent } from './routes/components/header/header.component';
+import { FormComponent } from './routes/components/form/form.component';
+import { IncomeComponent } from './routes/components/income/income.component';
+import { ExpensesComponent } from './routes/components/expenses/expenses.component';
+import { FooterComponent } from './routes/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FormComponent,
+    IncomeComponent,
+    ExpensesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
